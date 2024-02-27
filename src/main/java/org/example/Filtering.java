934b4354e6bfd3b9b8ee1f08663598389f194f37
Demo <demo.helloworld.1@gmail.com>
2024-02-27 15:15:03 +0530

    With and without output

--- src/main/java/org/example/Filtering.java
+++ src/main/java/org/example/Filtering.java
@@ -3,12 +3,11 @@ package org.example;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Filtering {
 
@@ -21,7 +20,7 @@ public class Filtering {
 
         @ProcessElement
         public void ProcessElement(ProcessContext c){
-            if (c.element()>threshold){
+            if (threshold < c.element()){
                 c.output(c.element());
             }
         }
@@ -33,9 +32,28 @@ public class Filtering {
 
         List<Double> googleStockPrice = Arrays.asList(1367.36, 1360.66, 1394.20, 1393.33, 1404.31, 1419.82, 1429.73);
 
+        /*The below 2 line code will be used to just run the transformations but output wont be available in terminal
         pipeline.apply(Create.of(googleStockPrice))
                 .apply(ParDo.of(new FilterThresholdFn(1400)));
+                */
 
+        pipeline.apply(Create.of(googleStockPrice))
+                .apply(MapElements.via(new SimpleFunction<Double, Double>() {
+
+                    @Override
+                    public Double apply(Double input) {
+                        System.out.println("-Pre Filtered = " + input);
+                        return input;
+                    }
+                }))
+                .apply(ParDo.of(new FilterThresholdFn(1400)))
+                .apply(MapElements.via(new SimpleFunction<Double, Void>() {
+                    @Override
+                    public Void apply(Double input){
+                        System.out.println("-Post Filtered = "+input);
+                        return null;
+                    }
+                }));
         pipeline.run();
     }
 }
